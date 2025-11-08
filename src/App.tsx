f8ee39d0ac871ebef5b84455ce7499ca418f48fc
masarumatsunaga <65140122+masarumatsunaga@users.noreply.github.com>
2025-11-08 18:49:38 +0900

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+import { useState } from 'react';
+import './App.css';
+import Main from './components/Main';
+import Sidebar from './components/Sidebar';
+import type { Note } from './types/Note';
+import uuid from 'react-uuid';
+
+function App() {
+  const [notes, setNotes] = useState<Note[]>([]);
+  const [activeNote, setActiveNote] = useState<string | false>(false);
+
+  const onAddNote = () => {
+    console.log('新しくノートが追加されました');
+    const newNote: Note = {
+      id: uuid(),
+      title: '新しいノート',
+      content: '新しいノートの内容',
+      modDate: Date.now(),
+    };
+    setNotes([...notes, newNote]);
+    console.log(notes);
+  };
+
+  const onDeleteNote = (id: string) => {
+    const fileterNotes = notes.filter((note) => note.id !== id);
+    setNotes(fileterNotes);
+    console.log(`{id}のノートが削除されました`);
+  };
+
+  return (
+    <div className="App">
+      <Sidebar
+        onAddNote={onAddNote}
+        onDeleteNote={onDeleteNote}
+        notes={notes}
+        activeNote={activeNote}
+        setActiveNote={setActiveNote}
+      />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,51 @@
+import './Sidebar.css';
+import type { Note } from '../types/Note';
+
+const Sidebar = ({
+  onAddNote,
+  onDeleteNote,
+  notes,
+  activeNote,
+  setActiveNote,
+}: {
+  onAddNote: () => void;
+  onDeleteNote: (id: string) => void;
+  notes: Note[];
+  activeNote: string | false;
+  setActiveNote: (id: string | false) => void;
+}) => {
+  return (
+    <div className="app-sidebar">
+      <div className="app-sidebar-header">
+        <h1>ノート</h1>
+        <button onClick={onAddNote}>追加</button>
+      </div>
+      <div className="app-sidebar-notes">
+        {notes.map((note) => (
+          <div
+            className={`app-sidebar-note ${
+              note.id === activeNote ? 'active' : ''
+            }`}
+            key={note.id}
+            onClick={() => setActiveNote(note.id)}
+          >
+            <div className="sidebar-note-title">
+              <strong>{note.title}</strong>
+              <button onClick={() => onDeleteNote(note.id)}>削除</button>
+            </div>
+            <p>{note.content}</p>
+            <small>
+              最後の修正日:
+              {new Date(note.modDate).toLocaleDateString('ja-JP', {
+                hour: '2-digit',
+                minute: '2-digit',
+              })}
+            </small>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
